e9abd10119d285530d61620908cb899d126d4341
Muhammad Afrizale <141467689+zallengrad@users.noreply.github.com>
2025-05-24 00:27:35 +0700

    revisi bismillah

--- src/server.js
+++ src/server.js
@@ -1,29 +1,23 @@
 const Hapi = require('@hapi/hapi');
-const userRoutes = require('./api/users/routes');
-const userHandler = require('./api/users/handler');
-const kameraRoutes = require('./api/kamera/routes');
-const kameraHandler = require('./api/kamera/handler');
-const riwayatRoutes = require('./api/riwayat/routes');
-const riwayatHandler = require('./api/riwayat/handler');
-
-
 
 const init = async () => {
-    const server = Hapi.server({
-        port: process.env.PORT || 3000,
-        host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
-        routes: {
-          cors: {
-            origin: ['*'],
-          },
-        },
-      });
-      
+  const server = Hapi.server({
+    port: process.env.PORT || 3000,
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  const userRoutes = require('./api/users/routes');
+  const userHandler = require('./api/users/handler');
+  const riwayatRoutes = require('./api/riwayat/routes');
+  const riwayatHandler = require('./api/riwayat/handler');
 
   server.route(userRoutes(userHandler));
-  server.route(kameraRoutes(kameraHandler));
   server.route(riwayatRoutes(riwayatHandler));
-
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
