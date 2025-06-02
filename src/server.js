104135af8d126146bad4e740d4e1ab54e482fe2f
Muhammad Afrizale <141467689+zallengrad@users.noreply.github.com>
2025-06-02 22:04:01 +0700

    membuat CORS origin

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ const historyRoutes = require('./api/histories/historyRoutes');
 const init = async () => {
   const server = Hapi.server({
     port: 3000,
-    host: 'localhost',
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
       cors: {
         origin: ['*'], 
